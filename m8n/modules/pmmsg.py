0d77d8f55cb02b7134732aebad93b3845600a233
Alolebot <135697169+Alolebot@users.noreply.github.com>
2023-06-07 14:56:47 +0300

    Update pmmsg.py

--- m8n/modules/pmmsg.py
+++ m8n/modules/pmmsg.py
@@ -11,5 +11,5 @@ from m8n.config import (
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"هلا حبيبي , هذا رد تلقائي الحساب خاص للمساعد اذا عندك شي راسل المطور\n\n‹ اتمنى ماتدز رسائل مزعجة ›")
+  await USER.send_message(message.chat.id,"هلا حبيبي ,  تم تفعيل البوت بواسطة توم @q5_u8  \n\n‹ اتمنى ماتدز رسائل مزعجة ›")
   return
